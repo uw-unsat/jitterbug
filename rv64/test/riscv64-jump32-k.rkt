79b52c0635d9dfd0ad0b7082058784b3352fd504
Luke Nelson <lukenels@cs.washington.edu>
2019-12-07 13:18:54 -0800

    rv64 ebpf jit: BPF_JMP32 BPF_K

---
+++ rv64/test/riscv64-jump32-k.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require "../bpf_jit_riscv64.rkt")
+(require serval/lib/unittest)
+
+(define tests
+  (test-suite+
+    "riscv64-jump32-k tests"
+    (jit-test-case '(BPF_JMP32 BPF_JSET BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JEQ BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JGT BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JLT BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JGE BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JLE BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JNE BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JSGT BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JSLT BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JSGE BPF_K))
+    (jit-test-case '(BPF_JMP32 BPF_JSLE BPF_K))
+))
+
+(module+ test
+  (time (run-tests tests)))
