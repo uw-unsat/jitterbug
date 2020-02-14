87601d483bea491bb71e76b23c3812bb8642ea61
Luke Nelson <lukenels@cs.washington.edu>
2020-02-14 15:45:06 -0800

    Fix annoying bug.
    Something somewhere is depending on an optimization that happens
    when choose* only has two parameters but not 3.  This patches over
    the problem to make verification work.

--- racket/lib/bpf-common.rkt
+++ racket/lib/bpf-common.rkt
@@ -140,8 +140,8 @@
     #:assumptions [assumptions (thunk null)])
 
     ; Assume dst and src each are either 0 or 6
-    (define dst (choose* 0 1 2 6))
-    (define src (choose* 0 1 2 6))
+    (define dst (choose* 0 3 6))
+    (define src (choose* 0 3 6))
 
     ; Create a symbolic register content for each BPF register
     (define-symbolic* r0 r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 (bitvector 64))
@@ -256,7 +256,9 @@
     ; triggered by division by zero for example. For now it should suffice to guard with "when"
     (when pre
 
-      (run-jit insn code dst src off imm ctx)
+      (for/all ([src src #:exhaustive])
+        (for/all ([dst dst #:exhaustive])
+          (run-jit insn code dst src off imm ctx)))
       (define insns (context-insns ctx))
 
       ; The next instruction is in the right place
