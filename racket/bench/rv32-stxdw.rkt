06904a157065d43341feee48761b252aa7b7b388
Luke Nelson <lukenels@cs.washington.edu>
2020-10-29 11:21:47 -0700

    Kill old bench directory

--- racket/bench/rv32-stxdw.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-
-(require
-  "../lib/tests.rkt"
-  (only-in "../rv32/spec.rkt" check-jit))
-
-(module+ test
-  (time (verify-stx-mem "rv32 stx dw benchmark" check-jit
-         #:selector (verify-only-in '(BPF_DW)))))
